9bf435708c007eb6eec08248fc37176d63bcdb86
U-WIT\luih <luih@WIT45000398.wit.private>
2018-12-18 18:22:55 -0500

    Dynamically creates markers from state array

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import MapContainer from "./components/MapContainer";
 import FooterContainer from "./components/FooterContainer";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      lights: [
+        {
+          lat: 42.3601,
+          lng: -71.0589
+        },
+        {
+          lat: 42.3605,
+          lng: -71.0589
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -23,10 +45,24 @@ class App extends Component {
               zoom: 14
             }}
             onMapLoad={map => {
+              /* Single Marker
               var marker = new window.google.maps.Marker({
                 position: { lat: 42.3601, lng: -71.0589 },
                 map: map,
-                title: "Hello Boston!"
+                icon: {
+                  url: "https://static.thenounproject.com/png/161116-200.png",
+                  scaledSize: new window.google.maps.Size(50, 50)
+                }
+              }); */
+              this.state.lights.forEach(function(light) {
+                var marker = new window.google.maps.Marker({
+                  position: { lat: light.lat, lng: light.lng },
+                  icon: {
+                    url: "https://static.thenounproject.com/png/161116-200.png",
+                    scaledSize: new window.google.maps.Size(50, 50)
+                  },
+                  map: map
+                });
               });
             }}
           />
